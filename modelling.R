1d7258d9c5720e2c1c430087b2960793f7058ed4
jashshah <jashshah92@gmail.com>
2017-07-04 16:38:32 +0530

    latest file

--- modelling.R
+++ modelling.R
@@ -10,6 +10,8 @@ glm.model <- glm(formula = quit ~ .,
 
 summary(glm.model) # AIC: 668.58
 
+write.csv(summary(glm.model)$coefficients, 'model_with_variance_and_rating.csv')
+
 pred_prob_test_glm <- predict(glm.model, newdata = x_test, type = 'response')
 
 pred_vals_test_glm <- as.factor(ifelse(pred_prob_test_glm >= 0.5, 1, 0))
@@ -24,7 +26,7 @@ roc(y_test$quit, pred_prob_test_glm)$auc # 0.8724
 
 write.csv(summary(glm.model)$coefficients, 'model_with_variance_and_rating.csv')
 
-### Model with Variance and Rating ###
+### Model without Variance and Rating ###
 
 glm.model <- glm(formula = quit ~ .-Rating -hike, 
                  data = data.frame(x_train, y_train), 
